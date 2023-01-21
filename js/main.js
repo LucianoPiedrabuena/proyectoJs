69c1fc71bfb8e20f7a15502975324221e13e65a9
Luciano Piedrabuena <piedrabuenaluciano@hotmail.com>
2023-01-21 18:47:14 -0300

    Entrega fina JS 1

--- js/main.js
+++ js/main.js
@@ -1,42 +1,46 @@
 
 
 
-let usuario = "Luciano";
-let contrasenia = 1234;
-let nombresIngresados = "";
-let ingresarUsuario = "";
-let contraseniaIngresada = "";
+// let usuario = "Luciano";
+// let contrasenia = 1234;
+// let nombresIngresados = "";
+// let ingresarUsuario = "";
+// let contraseniaIngresada = "";
 
-do {
- ingresarUsuario = prompt("Ingresá tu usuario, por favor");
- contraseniaIngresada = parseInt(prompt("Ahora, ingresá tu clave")); 
+// do {
+//  ingresarUsuario = prompt("Ingresá tu usuario, por favor");
+//  contraseniaIngresada = parseInt(prompt("Ahora, ingresá tu clave")); 
 
-} while (ingresarUsuario != usuario || contraseniaIngresada !=contrasenia);
+// } while (ingresarUsuario != usuario || contraseniaIngresada !=contrasenia)
 
-alert ("Bienvenido. Podés generar tu PTW");
-
-
-// SEGUNDA PREENTREGA
-//Mi idea es que al ingresar algún elemento del array tipoPermisos, me pregunte qué peligros existen.
-
-// function nuevoIngreso() {
-//  let ingreso = prompt("¿Qué tipo de permiso querés abrir?");
-//  const tipoPermisos = ["General", "Caliente", "Altura", "Excavaciones", "Espacios Confinados", "Eléctrico", "Izaje", "Demolición"];
+// Swal.fire({
+//     title: 'ACCESO CORRECTO',
+//     text: 'Puedes completar los campos del Permiso de Trabajo',
+//     icon: 'success',
+//     confirmButtonText: 'MUY BIEN. EMPECEMOS'
+//   })
 
 
-// }
-// nuevoIngreso ();
-// if (nuevoIngreso.ingreso == "general"){
-//     prompt("Ingresa el peligro")
-// }
 
-// const peligrosMecanicos = ["Objetos que caen", "Objetos cortantes", "Objetos que se proyectan", "Altas temperaturas", "Atrapamientos", "Choque de objetos", "Atropellamiento", "Volcamiento", "Resbalamiento", "Caídas a nivel", "Caídas a desnivel"]
-// const peligrosElectricos = ["Contacto eléctrico directo", "Contacto eléctrico indirecto", "Energía estática"]
-// const peligrosErgonomicos = ["Manipulación de cargas", "Levantamiento de carga", "Movimiento repetitivo", "Posturas forzadas"]
-// const peligrosQuimicos = ["Inhalación de polvos", "Vapores/gases", "Contacto Químico", "Otro"]
-// const peligrosFisicos = ["Ruidos/Vibraciones", "Stres térmico", "Iluminación", "Salpicaduras", "Ventilación", "Otros"]
-// const peligrosIncendio = ["Incendio de sólidos", "Incendio de combustibles", "Incendio de gases", "Incendio eléctrico", "Derrame químicos", "Incendios combinados"]
-// const peligrosBiologicos = ["Micoorganismos", "Insectos", "Otros"]
+
+const peligrosMecanicos = ["Objetos que caen", "Objetos cortantes", "Objetos que se proyectan", "Altas temperaturas", "Atrapamientos", "Choque de objetos", "Atropellamiento", "Volcamiento", "Resbalamiento", "Caídas a nivel", "Caídas a desnivel"]
+const peligrosElectricos = ["Contacto eléctrico directo", "Contacto eléctrico indirecto", "Energía estática"]
+const peligrosErgonomicos = ["Manipulación de cargas", "Levantamiento de carga", "Movimiento repetitivo", "Posturas forzadas"]
+const peligrosQuimicos = ["Inhalación de polvos", "Vapores/gases", "Contacto Químico", "Otro"]
+const peligrosFisicos = ["Ruidos/Vibraciones", "Stres térmico", "Iluminación", "Salpicaduras", "Ventilación", "Otros"]
+const peligrosIncendio = ["Incendio de sólidos", "Incendio de combustibles", "Incendio de gases", "Incendio eléctrico", "Derrame químicos", "Incendios combinados"]
+const peligrosBiologicos = ["Micoorganismos", "Insectos", "Otros"]
+
+const todosLosPeligros =[
+    [],
+    peligrosMecanicos,
+    peligrosElectricos,
+    peligrosErgonomicos,
+    peligrosQuimicos,
+    peligrosFisicos,
+    peligrosIncendio,
+    peligrosBiologicos,
+]
 // //  if(tipoPermisos.indexOf(ingreso) != -1) {
 // //     alert("Ingresá el tipo de permiso");
 // //  }
@@ -44,7 +48,7 @@ alert ("Bienvenido. Podés generar tu PTW");
  
 
 // console.log (peligrosGenerales);
- 
+ //Se intenta armar una "base de datos" en el localstorage. Se anula lo hecho en la segunda preentrega.
 localStorage.setItem("pMecanicos",["Objetos que caen","Objetos cortantes","Objetos que se proyectan","Altas temperaturas","Atrapamientos","Choque de objetos","Atropellamientos","Volcamiento","Resbalamiento"]);
 localStorage.setItem("pElectricos",["Contacto eléctrico directo", "Contacto eléctrico indirecto", "Energía estática"]);
 localStorage.setItem("pErgonomicos",["Manipulación de cargas", "Levantamiento de carga", "Movimiento repetitivo", "Posturas forzadas"]);
@@ -52,3 +56,51 @@ localStorage.setItem("pQuimicos",["Inhalación de polvos", "Vapores/gases", "Con
 localStorage.setItem("Pfisicos",["Ruidos/Vibraciones", "Stres térmico", "Iluminación", "Salpicaduras", "Ventilación", "Otros"]);
 localStorage.setItem("pIncendio",["Incendio de sólidos", "Incendio de combustibles", "Incendio de gases", "Incendio eléctrico", "Derrame químicos", "Incendios combinados"]);
 localStorage.setItem("pBiologicos",["Micoorganismos", "Insectos", "Otros"]);
+
+
+
+const boton = document.getElementById('btn');
+const enviarInfo = boton.onclick = ()=>{
+    
+}
+
+class Permiso{
+  empezarPermiso (tipo, medidas, epps){
+    const infoPermiso = {
+        tipo: tipo,
+        medidas : medidas,
+        epps : epps,
+    }
+  this.guardarPermisoEnLocalStorage(infoPermiso)  
+    }
+    guardarPermisoEnLocalStorage(infoPermiso){
+        let permiso;
+
+        permiso = this.obtenerPermisoLocalStorage();
+
+        permiso.push(infoPermiso);
+
+        localStorage.setItem('permiso', JSON.stringify(permiso))
+    }
+    obtenerPermisoLocalStorage(){
+        permisoLs;
+
+        if(localStorage.getItem('permiso')===null){
+            permisoLs =[];
+
+        }
+        else{
+        permisoLs = JSON.parse(localStorage.getItem ('permiso'))
+        }
+        return permisoLs;
+    }
+
+}
+
+
+
+
+
+
+
+
